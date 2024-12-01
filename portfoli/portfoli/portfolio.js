f3dfd88fd909e7ed0003c8acb6a1728c3caf445c
Srujana katla <116832145+Srujanakatla@users.noreply.github.com>
2024-12-01 17:20:57 +0530

    Add files via upload

---
+++ portfoli/portfoli/portfolio.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".text", {
+    strings: ["Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+             
+             
+             
+             
